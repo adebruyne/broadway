310e61284cf8b71d472e5c4112e8f6b11a23e7f9
Aylin DeBruyne <adebruyne88@gmail.com>
2019-04-08 07:03:28 -0400

    finishes create action

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,2 +1,25 @@
 class PlaysController < ApplicationController
+    def index
+    end
+
+    def new
+        @play = Play.new
+    end
+
+    def create
+        @play = Play.new(play_params)
+
+        if @play.save
+            redirect_to root_path
+        else 
+            render 'new'
+    
+        end
+    end
+
+    private
+
+    def play_params
+        params.require(:play).permit(:title, :description, :director)
+    end
 end
